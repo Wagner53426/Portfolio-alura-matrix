43873746318d03c9ed18e68fac0dd5dccdcc3408
Wagner53426 <wagner6528@gmail.com>
2022-01-25 21:25:09 -0300

    alteraçao no projeto

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +33,12 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'Wagner53426';
+ // const username = 'Wagner53426';
+  const [username, setUsername] = React.useState('Wagner53426');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +65,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento){
+              infosDoEvento.preventDefault();
+              console.log("Alguem submeteu o form");
+              roteamento.push('/chat')
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,7 +80,24 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+           {/* <input type="text"
+            value={username}
+            onChange={function (event){
+              //onde está o valor
+              const valor = event.target.value;
+              //Trocar o valor da varíavel através do react
+              setUsername(valor);
+            }}
+          /> */}
+
             <TextField
+             value={username}
+             onChange={function (event){
+              //onde está o valor
+              const valor = event.target.value;
+              //Trocar o valor da varíavel através do react
+              setUsername(valor);
+            }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -111,7 +107,7 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
-            />
+            /> 
             <Button
               type='submit'
               label='Entrar'
